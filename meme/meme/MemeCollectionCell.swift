160be8b54ecd692ba8b2c66c5abc6774419bac08
itornaza <itornaza@users.noreply.github.com>
2015-04-02 22:34:53 +0300

    Committed by Udacity

---
+++ meme/meme/MemeCollectionCell.swift
@@ -0,0 +1,17 @@
+//
+//  MemeCollectionCell.swift
+//  meme
+//
+//  Created by Ioannis Tornazakis on 28/3/15.
+//  Copyright (c) 2015 polarbear.gr. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionCell: UICollectionViewCell {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+    
+}
